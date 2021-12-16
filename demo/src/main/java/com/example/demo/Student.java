5bbec2e8eeac8b97daa5a51363bbc6caf68ef0a0
“valberjr” <3cifhzht@duck.com>
2021-12-16 16:03:09 -0300

    @document and @id mapping

--- demo/src/main/java/com/example/demo/Student.java
+++ demo/src/main/java/com/example/demo/Student.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.List;
 
 @Data
+@Document
 public class Student {
+    @Id
+    private String id;
     private String firstName;
     private String lastName;
     private String email;
