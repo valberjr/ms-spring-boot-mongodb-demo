d4a7e9abeb641952a7a63d80d13bd7b89ca86ede
“valberjr” <3cifhzht@duck.com>
2021-12-16 16:21:43 -0300

    document creation

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(StudentRepository repository) {
+        return args -> {
+            Adress adress = new Adress(
+                    "England",
+                    "London",
+                    "NE9"
+            );
+
+            Student student = new Student(
+                    "Jess",
+                    "P",
+                    "jess@email.com",
+                    Gender.FEMALE,
+                    adress,
+                    List.of("Computer Science", "Maths"),
+                    BigDecimal.TEN,
+                    LocalDateTime.now()
+            );
+
+            repository.insert(student);
+        };
+    }
 
 }
