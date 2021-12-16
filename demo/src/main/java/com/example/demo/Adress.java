de9e600f85f00aa2a2001e7f9d01ecf94ba3b986
“valberjr” <3cifhzht@duck.com>
2021-12-16 16:21:27 -0300

    constructor added

--- demo/src/main/java/com/example/demo/Adress.java
+++ demo/src/main/java/com/example/demo/Adress.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Adress {
     private String country;
     private String city;
