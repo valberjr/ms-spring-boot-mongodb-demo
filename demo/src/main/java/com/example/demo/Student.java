9de1fa2473fd21f1052c73009dc095603f5c57a1
“valberjr” <3cifhzht@duck.com>
2021-12-16 16:19:30 -0300

    fix local date time

--- demo/src/main/java/com/example/demo/Student.java
+++ demo/src/main/java/com/example/demo/Student.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
-import java.time.ZonedDateTime;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -20,5 +20,25 @@ public class Student {
     private Adress adress;
     private List<String> favoriteSubjects;
     private BigDecimal totalSpendInBooks;
-    private ZonedDateTime created;
+    private LocalDateTime created;
+
+    public Student(
+            String firstName,
+            String lastName,
+            String email,
+            Gender gender,
+            Adress adress,
+            List<String> favoriteSubjects,
+            BigDecimal totalSpendInBooks,
+            LocalDateTime created
+    ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.adress = adress;
+        this.favoriteSubjects = favoriteSubjects;
+        this.totalSpendInBooks = totalSpendInBooks;
+        this.created = created;
+    }
 }
