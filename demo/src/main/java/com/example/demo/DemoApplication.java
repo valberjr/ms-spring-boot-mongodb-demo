cdb9f18ecffddec7a8a8db0ee131429a513fc4da
“iorjun” <3cifhzht@duck.com>
2021-12-17 09:23:01 -0300

    queries from method names

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -20,10 +17,7 @@ public class DemoApplication {
     }
 
     @Bean
-    CommandLineRunner runner(
-            StudentRepository repository,
-            MongoTemplate mongoTemplate
-    ) {
+    CommandLineRunner runner(StudentRepository repository) {
         return args -> {
             Adress adress = new Adress(
                     "England",
@@ -43,23 +37,11 @@ public class DemoApplication {
                     LocalDateTime.now()
             );
 
-            Query query = new Query();
-            query.addCriteria(Criteria.where("email").is(email));
-
-            List<Student> students = mongoTemplate.find(query, Student.class);
-
-            if (students.size() > 1) {
-                throw new IllegalStateException(
-                        "found many students with email " + email);
-            }
-
-            if (students.isEmpty()) {
-                System.out.println("Inserting student " + student);
-                repository.insert(student);
-            } else {
-                System.out.println(student + " already exists");
-            }
-
+            repository.findStudentByEmail(email)
+                    .ifPresentOrElse(s -> System.out.println(s + " already exists"), () -> {
+                        System.out.println("Inserting student " + student);
+                        repository.insert(student);
+                    });
         };
     }
 
